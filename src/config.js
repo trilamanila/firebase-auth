d7eda8fd2ce1b14bc9778fed29e8f7b462983d0a
JC <jctrilamanila@gmail.com>
2021-05-24 21:44:22 -0400

    created project

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyByPcsQzLyXAKSUNYkeksmyBLgOwnJf-BA",
+  authDomain: "react-firebase-auth-8be6c.firebaseapp.com",
+  projectId: "react-firebase-auth-8be6c",
+  storageBucket: "react-firebase-auth-8be6c.appspot.com",
+  messagingSenderId: "408159409893",
+  appId: "1:408159409893:web:a9659bf427b8058dede60c"
+};
